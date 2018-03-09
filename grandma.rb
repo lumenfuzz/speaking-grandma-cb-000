a7928e950bffa6ef222067be6db46d0905b6b816
Patrick James Louis <lumenfuzz@gmail.com>
2018-03-09 17:36:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,7 @@ require 'pry'
 
 def speak_to_grandma(phrase)
 
-  binding.pry
   if (phrase != phrase.upcase)
-    binding.pry
      puts "HUH?! SPEAK UP, SONNY"
    elsif (phrase == "I LOVE YOU GRANDMA!")
      puts "I LOVE YOU TOO PUMPKIN!"
