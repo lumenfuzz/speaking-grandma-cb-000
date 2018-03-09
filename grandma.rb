b3fe224c47b6eab05d9a88efefe65c8dbebaa316
Patrick James Louis <lumenfuzz@gmail.com>
2018-03-09 17:37:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
 
   if (phrase != phrase.upcase)
-    "HUH?! SPEAK UP, SONNY"
+    "HUH?! SPEAK UP, SONNY!"
    elsif (phrase == "I LOVE YOU GRANDMA!")
     "I LOVE YOU TOO PUMPKIN!"
    else
